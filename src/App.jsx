690172397dbf9e9d34ed19ac872210fa522fceda
leejiwon <365support@naver.com>
2022-10-27 11:45:16 +0900

    Refactor: Suspense 성능 개선

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+/** @jsxImportSource @emotion/react */
 import { Routes, Route } from "react-router-dom";
 import Auth from "./pages/Auth";
 import { lazy, Suspense } from "react";
 import "react-toastify/dist/ReactToastify.css";
+import { mainContainer } from "./shared/globalStyle";
 
 const Todo = lazy(() => import("./pages/Todo"));
 
@@ -13,7 +15,7 @@ function App() {
       <Route
         path="/todo"
         element={
-          <Suspense fallback={<div>...로딩중</div>}>
+          <Suspense fallback={<div css={mainContainer}>...로딩중</div>}>
             <Todo />
           </Suspense>
         }
